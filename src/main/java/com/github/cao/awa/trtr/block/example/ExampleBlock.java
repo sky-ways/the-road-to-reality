682796a6f92f0ade9915919da44744203296d0fc
cao_awa <3313220756@qq.com>
2023-03-15 23:53:54 +0800

    Changes in example block.

--- src/main/java/com/github/cao/awa/trtr/block/example/ExampleBlock.java
+++ src/main/java/com/github/cao/awa/trtr/block/example/ExampleBlock.java
@@ -4,6 +4,7 @@ import com.github.cao.awa.apricot.anntations.Auto;
 import com.github.cao.awa.trtr.annotations.DataGen;
 import com.github.cao.awa.trtr.block.TrtrBlock;
 import com.github.cao.awa.trtr.block.example.loot.ExampleLoot;
+import com.github.cao.awa.trtr.block.example.model.ExampleModel;
 import com.github.cao.awa.trtr.framework.data.gen.loot.LootFactory;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Material;
@@ -40,6 +41,14 @@ public class ExampleBlock extends TrtrBlock {
     // Here one block item is invalid, the name must be "ITEM" or "BLOCK_ITEM", framework will ignore it automatically.
     // Direct item with class.
     public static final Class<ExampleBlockItem> TYPE_ITEM = ExampleBlockItem.class;
+
+    // Models.
+    // Direct model provider
+    public static ExampleModel MODEL;
+
+    // Here one block model provider is invalid, the name must be "MODEL" or "MODEL_PROVIDER", framework will ignore it automatically.
+    // Direct item with class.
+    public static final Class<ExampleModel> TYPE_MODEL = ExampleModel.class;
 
     // Constructor...
     public ExampleBlock(Settings settings) {
--- src/main/java/com/github/cao/awa/trtr/block/example/model/ExampleModel.java
+++ src/main/java/com/github/cao/awa/trtr/block/example/model/ExampleModel.java
@@ -1,29 +1,28 @@
-package com.github.cao.awa.trtr.data.gen.model;
+package com.github.cao.awa.trtr.block.example.model;
 
+import com.github.cao.awa.trtr.block.TrtrBlocks;
+import com.github.cao.awa.trtr.block.example.ExampleBlock;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
-import net.minecraft.block.Block;
 import net.minecraft.data.client.BlockStateModelGenerator;
 import net.minecraft.data.client.ItemModelGenerator;
 import net.minecraft.data.client.Models;
 import net.minecraft.item.Item;
 
-public class GenericModelProvider extends FabricModelProvider {
-    private final Block block;
-
-    public GenericModelProvider(FabricDataOutput output, Block block) {
+public class ExampleModel extends FabricModelProvider {
+    public ExampleModel(FabricDataOutput output) {
         super(output);
-        this.block = block;
     }
 
     @Override
     public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
-        blockStateModelGenerator.registerSimpleCubeAll(this.block);
+        System.out.println("Custom model generator");
+        blockStateModelGenerator.registerSimpleCubeAll(TrtrBlocks.get(ExampleBlock.IDENTIFIER));
     }
 
     @Override
     public void generateItemModels(ItemModelGenerator itemModelGenerator) {
-        Item item = this.block.asItem();
+        Item item = TrtrBlocks.get(ExampleBlock.IDENTIFIER).asItem();
         if (item != null) {
             itemModelGenerator.register(item,
                                         Models.GENERATED
