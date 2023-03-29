2ec0b16d30bcb5bc753327641b49b0c5a408deb4
cao_awa <3313220756@qq.com>
2023-03-29 20:17:46 +0800

    Add halite block. Removed useless language translate key.

---
+++ src/main/java/com/github/cao/awa/trtr/block/ore/salt/halite/Halite.java
@@ -0,0 +1,43 @@
+package com.github.cao.awa.trtr.block.ore.salt.halite;
+
+import com.github.cao.awa.apricot.anntation.Auto;
+import com.github.cao.awa.trtr.annotation.data.gen.DataGen;
+import com.github.cao.awa.trtr.annotation.mine.PickaxeMining;
+import com.github.cao.awa.trtr.block.TrtrBlock;
+import com.github.cao.awa.trtr.data.gen.loot.GenericBlockLootProvider;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.yarn.constants.MiningLevels;
+import net.minecraft.block.MapColor;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.util.Identifier;
+
+@Auto
+@PickaxeMining(MiningLevels.STONE)
+public class Halite extends TrtrBlock {
+    @Auto
+    public static final Identifier IDENTIFIER = Identifier.of("trtr",
+                                                              "halite"
+    );
+
+    @Auto
+    public static final FabricBlockSettings SETTINGS = FabricBlockSettings.of(Material.STONE,
+                                                                              MapColor.WHITE
+                                                                          )
+                                                                          .requiresTool()
+                                                                          .strength(1.5F,
+                                                                                    6.0F
+                                                                          );
+
+    @Auto
+    public static BlockItem ITEM;
+
+    @Auto
+    @DataGen
+    public static GenericBlockLootProvider LOOT;
+
+    @Auto
+    public Halite(Settings settings) {
+        super(settings);
+    }
+}
