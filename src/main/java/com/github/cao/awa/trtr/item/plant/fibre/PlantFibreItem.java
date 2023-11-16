da4a6546c00b20cb67bd9a83637fc565db0ab6d9
cao_awa <cao.awa.www@gmail.com>
2023-11-16 15:37:34 +0800

    Plant fibre.

--- src/main/java/com/github/cao/awa/trtr/item/plant/fibre/PlantFibreItem.java
+++ src/main/java/com/github/cao/awa/trtr/item/plant/fibre/PlantFibreItem.java
@@ -1,10 +1,16 @@
 package com.github.cao.awa.trtr.item.plant.fibre;
 
 import com.github.cao.awa.apricot.anntation.Auto;
+import com.github.cao.awa.trtr.item.craft.CraftingItem;
 import net.minecraft.util.Identifier;
 
 @Auto
-public class PlantFibreItem {
+public class PlantFibreItem extends CraftingItem {
     @Auto
     public static final Identifier IDENTIFIER = Identifier.tryParse("trtr:plant_fibre");
+
+    @Auto
+    public PlantFibreItem(Settings settings) {
+        super(settings);
+    }
 }
