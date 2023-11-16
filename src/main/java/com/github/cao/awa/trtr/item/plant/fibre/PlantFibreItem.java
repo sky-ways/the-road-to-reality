b01d21b90f1a2d5b85cf7e3b022ea66cd7ecd18e
cao_awa <cao.awa.www@gmail.com>
2023-11-16 14:27:12 +0800

    Plant fibre.

---
+++ src/main/java/com/github/cao/awa/trtr/item/plant/fibre/PlantFibreItem.java
@@ -0,0 +1,10 @@
+package com.github.cao.awa.trtr.item.plant.fibre;
+
+import com.github.cao.awa.apricot.anntation.Auto;
+import net.minecraft.util.Identifier;
+
+@Auto
+public class PlantFibreItem {
+    @Auto
+    public static final Identifier IDENTIFIER = Identifier.tryParse("trtr:plant_fibre");
+}
